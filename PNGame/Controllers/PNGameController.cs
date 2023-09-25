ba02d5deef72835a65ec7d53fe19dbd4a2b7800a
woonho <kumo86@gmail.com>
2023-09-26 00:21:21 +0900

    기본 컨트롤러 생성

---
+++ PNGame/Controllers/PNGameController.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+using PNGame.ActionFilters;
+using PNUnity.Share;
+
+namespace PNGame.Controllers;
+
+[Route("[controller]/[action]")]
+[FileLogFilter]
+public class PNGameController : ControllerBase
+{
+    protected static IActionResult Error(ErrorCode ec, [CallerFilePath] string filepath = "", [CallerLineNumber] int line = 0)
+    {
+        return new JsonResult(new
+        {
+            code = (int)ec,
+            debug = $"{ec} {Path.GetFileName(filepath)}:{line}",
+        });
+    }
+
+    protected static IActionResult Success(object response)
+    {
+        return new JsonResult(response);
+    }
+}
