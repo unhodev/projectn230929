e40446cfb4d1a4b64600356a19fe596030839f41
woonho <kumo86@gmail.com>
2023-09-25 21:47:23 +0900

    프로젝트 시작

---
+++ PNGame/Controllers/RootController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PNGame.Controllers;
+
+[Route("[action]")]
+public class RootController : ControllerBase
+{
+    public IActionResult Health() => Ok(new { });
+}
